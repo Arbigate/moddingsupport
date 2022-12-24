d52daa4bec9b5659e77117862a019ed00b326315
Arbigate <100032895+Arbigate@users.noreply.github.com>
2022-12-24 00:02:38 -0500

    initial commit

---
+++ bot.py
@@ -0,0 +1,81 @@
+"""
+Copyright notice:
+
+Parts of this code are based on https://github.com/JonathanFeenstra/discord-modlinkbot:
+
+Copyright (C) 2019-2023 Jonathan Feenstra
+Copyright (C) 2022-2023 Arbigate
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import os
+import discord
+import aiohttp
+from discord.ext import commands
+from dotenv import load_dotenv
+from nexus_search import RequestHandler
+from cogs.modding_support import HelpCommand
+
+load_dotenv()
+extensions = ['cogs.modlink', 'cogs.modding_support']
+ERROR_CHANNEL_ID = 1056028135377874954
+ARCHITECT_CHANNEL = 1056028387413590056
+
+
+class ModLink(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix='-',
+            help_command=HelpCommand(),
+            intents=discord.Intents(
+                guilds=True, members=True, message_content=True, guild_messages=True
+            ),
+            )
+
+    async def setup_hook(self):
+        self.loop.create_task(self.startup())
+
+        for extension in extensions:
+            await bot.load_extension(extension)
+
+    async def startup(self):
+        self.session = aiohttp.ClientSession()
+        self.error_channel = self.get_channel(ERROR_CHANNEL_ID) or await self.fetch_channel(ERROR_CHANNEL_ID)
+        self.architect_channel = self.get_channel(ARCHITECT_CHANNEL) or await self.fetch_channel(ARCHITECT_CHANNEL)
+        await self.initialize_request_handler()
+
+    async def close(self):
+        await self.session.close()
+        await super().close()
+
+    async def initialize_request_handler(self):
+        self.request_handler = RequestHandler(self.session)
+
+
+bot = ModLink()
+
+
+def check_if_verified(ctx):
+    return ctx.message.author.id == 668828647653638174
+
+
+@bot.command(name="sync")
+@commands.check(check_if_verified)
+async def sync(ctx):
+    x = await bot.tree.sync()
+    print(x)
+    await ctx.send("Bot synced!")
+
+bot.run(os.getenv("TOKEN"))
---
+++ common_exceptions.py
@@ -0,0 +1,78 @@
+manual_exceptions = {
+    'lux': 1
+}
+
+false_nsfw_flagged = {
+    'enhanced blood textures':'https://www.nexusmods.com/skyrimspecialedition/mods/2357',
+    'glenmoril':'https://www.nexusmods.com/skyrimspecialedition/mods/32998',
+    'vigilant':'https://www.nexusmods.com/skyrimspecialedition/mods/11849/',
+    'sky idles':'https://www.nexusmods.com/skyrimspecialedition/mods/45794',
+}
+
+common_acronyms = {
+    'elfx': 'Enhanced Lights and FX',
+    'nff': 'Nether Follower Framework',
+    'ussep': 'Unofficial Skyrim Edition Patch',
+    'uslep': 'Unofficial Skyrim Edition Patch',
+    'mo2': 'Mod Organizer 2',
+    'lotd': 'Legacy of the Dragonborn',
+    'icow': 'Immersive College of Winterhold',
+    'nemesis': 'Nemesis Unlimited Behavior Engine',
+    'cgo': 'Combat Gameplay Overhaul',
+    'mbo': 'Movement Behavior Overhaul',
+    'jbo': 'Jump Behavior Overhaul',
+    'tdm': 'True Directional Movement',
+    'dar': 'Dynamic Animation Replacer'
+}
+
+sos_acronym = {
+    'Sabrecats of Skyrim': 'https://www.nexusmods.com/skyrim/mods/37719',
+    'Sales Overflow Solved': 'https://www.nexusmods.com/skyrimspecialedition/mods/41625',
+    'Scallions of Skyrim': 'https://www.nexusmods.com/skyrimspecialedition/mods/53205',
+    'Scarecrows of Skyrim': 'https://www.nexusmods.com/skyrimspecialedition/mods/39575',
+    'Scarves of Skyrim': 'https://www.nexusmods.com/skyrim/mods/16660/',
+    'Scribes of Skyrim': 'https://www.nexusmods.com/skyrimspecialedition/mods/50008',
+    'Sea of Spirits': 'https://www.nexusmods.com/skyrimspecialedition/mods/4781',
+    'Seagulls of Skyrim': 'https://www.nexusmods.com/skyrimspecialedition/mods/52153',
+    'Seasons of Skyrim': 'https://www.nexusmods.com/skyrimspecialedition/mods/62861',
+    'Shadows of Sunlight': 'https://www.nexusmods.com/skyrimspecialedition/mods/41368',
+    'Sharpen Other Swords': 'https://www.nexusmods.com/skyrimspecialedition/mods/52723',
+    'Sharpening Oversampling Shader': 'https://www.nexusmods.com/skyrimspecialedition/mods/45295',
+    'Shields of Skyrim': 'https://www.nexusmods.com/skyrim/mods/21988',
+    'Sigils of Skyrim': 'https://www.nexusmods.com/skyrimspecialedition/mods/54108',
+    'Signs of Skyrim': 'https://www.nexusmods.com/skyrimspecialedition/mods/22292',
+    'Simple Obvious Spell-crafting': 'https://www.nexusmods.com/skyrimspecialedition/mods/48621',
+    'Simple Offence Suppression': 'https://www.nexusmods.com/skyrimspecialedition/mods/41764',
+    'Simple Overwhelming Slaying': 'https://www.nexusmods.com/skyrimspecialedition/mods/46324',
+    'Simply Overflowing Salt': 'https://www.nexusmods.com/skyrimspecialedition/mods/48819/',
+    'Sink or Swim': 'https://www.nexusmods.com/skyrimspecialedition/mods/42962',
+    'Skaven of Skyrim': 'https://www.nexusmods.com/skyrimspecialedition/mods/46519',
+    'Skyrim Outfit System': 'https://www.nexusmods.com/skyrimspecialedition/mods/42162',
+    'Sleeping on Solstheim': 'https://www.nexusmods.com/skyrimspecialedition/mods/46236',
+    'Slip off Slopes': 'https://www.nexusmods.com/skyrimspecialedition/mods/45886',
+    'Sloads of Skyrim': 'https://www.nexusmods.com/skyrim/mods/21771',
+    'Smart Optimal Salves': 'https://www.nexusmods.com/skyrimspecialedition/mods/48479',
+    'Sojourn over Signs': 'https://www.nexusmods.com/skyrimspecialedition/mods/51567',
+    'Solstheim Objects SMIMed': 'https://www.nexusmods.com/skyrimspecialedition/mods/53779',
+    'Sounds of Skyrim': 'https://www.nexusmods.com/skyrimspecialedition/mods/8286?',
+    'Sprawling Open Solitude': 'https://www.nexusmods.com/skyrimspecialedition/mods/46432',
+    'Spread Our Spells': 'https://www.nexusmods.com/skyrimspecialedition/mods/46010',
+    'Staff of Shalidor': 'https://www.nexusmods.com/skyrimspecialedition/mods/13764',
+    'Staff of Sheogorath': 'https://www.nexusmods.com/skyrimspecialedition/mods/16873',
+    'Stances of Skyrim': 'https://www.nexusmods.com/skyrimspecialedition/mods/21617',
+    'Starting Outfit Suppressed': 'https://www.nexusmods.com/skyrimspecialedition/mods/43967',
+    'Staves of Skyrim': 'https://www.nexusmods.com/skyrim/mods/22691',
+    'Stockades of Skyrim': 'https://www.nexusmods.com/skyrimspecialedition/mods/43227',
+    'Storm of Steel': 'https://www.nexusmods.com/skyrimspecialedition/mods/33697',
+    'Supplies of Skyrim': 'https://www.nexusmods.com/skyrimspecialedition/mods/45377',
+    'Sweeping Organizes Stuff': 'https://www.nexusmods.com/skyrimspecialedition/mods/51645',
+    'Sword of Shadow': 'https://www.nexusmods.com/skyrimspecialedition/mods/60766'
+}
+
+gamepass_gif = [
+    'https://cdn.discordapp.com/attachments/806307229182984202/1016099201119174667/IMG_5618.gif',
+    'https://cdn.discordapp.com/attachments/806307229182984202/1016100862613016587/IMG_5620.gif',
+    'https://cdn.discordapp.com/attachments/806307229182984202/1016139114170564648/IMG_5622.gif',
+    'https://cdn.discordapp.com/attachments/806307229182984202/1016194553348370452/IMG_5625.gif',
+    'https://cdn.discordapp.com/attachments/806307229182984202/1016194832479297566/IMG_5626.gif'
+]
